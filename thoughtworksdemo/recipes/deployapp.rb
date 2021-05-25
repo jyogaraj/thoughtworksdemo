9ed66a0854525f5d71f122e8a6882987a3ae5c7f
Yogaraj <yogi.instruzz@gmail.com>
2021-05-25 19:23:00 +0530

    updates

--- thoughtworksdemo/recipes/deployapp.rb
+++ thoughtworksdemo/recipes/deployapp.rb
@@ -7,10 +7,10 @@
 require 'securerandom'
 random_string = SecureRandom.hex
 
-git "/tmp/#{random_string}" do
-  repository "https://github.com/jyogaraj/mediawiki-app.git"
-  revision "main"
-  action :sync
+remote_file "/tmp/#{random_string}/mediawiki-1.35.2.tar.gz" do
+  source 'https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.2.tar.gz'
+  mode '0755'
+  action :create
 end
 
 execute 'untar archive' do
@@ -28,6 +28,51 @@ ruby_block "replace document root" do
       fe.search_file_replace(/"\/var\/www\/html"/,"/var\/www\/\mw")
       fe.write_file
     end
+end
+
+layerID = ''
+dbhostip = ''
+search("aws_opsworks_layer").each do |layer|
+  Chef::Log.info("********** The layer's name is '#{layer['name']}' **********")
+  if layer['name'] == 'mariadb-layer'
+    layerID = layer['layer_id']
+  end
+end
+Chef::Log.info("********** The layer's id is '#{layerID}' **********")
+
+
+search("aws_opsworks_instance").each do |instance|
+    Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}' **********")
+    Chef::Log.info("#{instance['layer_ids']}")
+    if instance['layer_ids'].include?(layerID)
+        Chef::Log.info("#{instance['instance_id']} - #{instance['status']}")
+        if instance['status'] == 'online'
+            dbhostip = instance['private_ip']
+        end
+    end
+end
+
+instance = search("aws_opsworks_instance", "self:true").first
+
+#template '/var/www/mw/LocalSettings.php' do
+#    source 'LocalSettings.erb'
+#    owner 'root'
+#    variables(
+#      'db_host': dbhostip || node[:dbhost],
+#      'instance_public_dns': instance['public_dns']
+#    )
+#    action :create
+#end
+
+db_host = dbhostip || node[:dbhost]
+
+bash 'Install mediawiki' do
+  user 'root'
+  cwd  "#{node['mediawiki']['path']}"
+  code <<-EOH
+  /usr/bin/php #{node['mediawiki']['path']}/maintenance/install.php --conf #{node['mediawiki']['path']}/LocalSettings.php #{node['mediawiki']['title']} admin --pass #{node['mediawiki']['password']} --dbname wikidatabase --dbuser wiki --dbpass #{node['mysql']['wiki_user_password']} --dbserver #{dbhostip} --lang #{node['mediawiki']['lang']} --scriptpath '' --server http://#{instance['public_dns']}
+  EOH
+  not_if { ::File.exist?(node['mediawiki']['path'] + '/LocalSettings.php') }
 end
 
 service "httpd" do
